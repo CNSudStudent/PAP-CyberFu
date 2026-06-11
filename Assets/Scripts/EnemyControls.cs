ba5e790b169812a5cf23f6eac980a577ef47f25f
CNSudStudent <cnsudstudent1@gmail.com>
2026-06-11 18:39:53 -0400

    CyberFu

--- Assets/Scripts/EnemyControls.cs
+++ Assets/Scripts/EnemyControls.cs
@@ -1,51 +1,132 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyControls : MonoBehaviour
 {
-    public float speed = 2;
-    public float attackingDistance = 0.6f;
-    private Animator animatorEnemy;
-    private Rigidbody rigidbodyEnemy;
+    public float speed = 5f;
+    public float attackingDistance = 1f;
+
+    private Animator anim;
+    private Rigidbody rb;
     private Transform target;
-    [SerializeField]
-    private bool isFollowingTarget;
-    private float maxAttackingTime = 2;
-    private float currentAttackingTime=0f;
+    public Vector3 direction;
+    private float chasingPlayer = 0.01f;
+    private float currentAttackingTime;
+    private float maxAttackingTime = 2f;
+    private bool followTarget;
+    private bool attackTarget;
+    private GameObject playerModel;
 
     // Start is called before the first frame update
     void Start()
     {
-        animatorEnemy = GetComponent<Animator>();
-        rigidbodyEnemy = GetComponent<Rigidbody>();
-        target = GameObject. FindGameObjectWithTag("Player").transform;
+        playerModel = GameObject.Find("Generic Man");
+        followTarget = true;
+        currentAttackingTime = maxAttackingTime;
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player && player.activeInHierarchy)
+        {
+            target = player.transform;
+        } else
+        {
+            target = null;
+        }
+        
+    }
+
+    private void Update()
+    {
+        Attack();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-      transform.LookAt(target.position); 
-      isFollowingTarget = Vector3.Distance(transform.position,target.position) >= attackingDistance;
-      if(isFollowingTarget)
-        {
-            rigidbodyEnemy.velocity = transform.forward * speed;
-        }
-        else
-        {
-            Attack();
-        }
-        animatorEnemy.SetBool("Walk", isFollowingTarget);
+        FollowTarget();
     }
+
+    void FollowTarget()
+    {
+        if (!target) return;
+        if (!followTarget)
+        {
+            rb.isKinematic = true;
+            return;
+        }
+
+        if(!playerModel.activeInHierarchy){
+            return;
+        }
+
+        if(Vector3.Distance(transform.position, target.position) >= attackingDistance)
+        {
+            rb.isKinematic = false;
+            direction = target.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 100f);
+
+            if (rb.velocity.sqrMagnitude != 0)
+            {
+                rb.velocity = transform.forward * speed;
+                anim.SetBool("Walk", true);
+            }
+        }
+        else if(Vector3.Distance(transform.position, target.position) <= attackingDistance)
+        {
+            rb.isKinematic = false;
+            rb.velocity = Vector3.zero;
+            anim.SetBool("Walk", false);
+
+            followTarget = false;
+            attackTarget = true;
+        }
+    }
+
+    public void EnemyAttack(int attack)
+    {
+        if(attack == 0)
+        {
+            anim.SetTrigger("Attack1");
+        }
+
+        if (attack == 1)
+        {
+            anim.SetTrigger("Attack2");
+        }
+
+        if (attack == 2)
+        {
+            anim.SetTrigger("Attack3");
+        }
+    }
+
     void Attack()
     {
-        rigidbodyEnemy.velocity = Vector3.zero;
-        currentAttackingTime += Time.deltaTime;
-        if (currentAttackingTime > maxAttackingTime)
+        
+        if(!playerModel.activeInHierarchy){
+            return;
+        }
+        if (!attackTarget)
         {
+            return;
+        }
+
+        currentAttackingTime += Time.deltaTime;
+
+        if(currentAttackingTime > maxAttackingTime)
+        {
+            EnemyAttack(Random.Range(0, 3));
             currentAttackingTime = 0f;
-            int rand = Random.Range(1,7);
-            animatorEnemy.SetTrigger("Attack"+ rand );
+        }
+
+        if(Vector3.Distance(transform.position, target.position) > attackingDistance + chasingPlayer)
+        {
+            attackTarget = false;
+            followTarget = true;
         }
     }
 }
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxPlayerHealth = 10;
+    public int currentPlayerHealth;
+    public int enemyDamage = 2;
+    public PlayerExplosionParticles particles;
+    private Animator playerAnimator;
+    public void HurtPlayer()
+    {
+        currentPlayerHealth-= enemyDamage;
+        playerAnimator.SetTrigger("Hit");
+        if (currentPlayerHealth <= 0)
+        {
+            gameObject.SetActive(false);
+            particles.Explode();
+           Invoke("ReloadScene",5);
+        }
+    } 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HitCollider"))
+        {
+            HurtPlayer();
+        }
+        
+    }
+    void Start()
+    {
+        currentPlayerHealth = maxPlayerHealth;
+        playerAnimator = GetComponent<Animator>();
+        particles= GetComponent<PlayerExplosionParticles>();
+    }
+    private void ReloadScene(){
+            SceneManager.LoadScene("CyberFu");
+    }
+    
+}
